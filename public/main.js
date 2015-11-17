9073bf1ce0b049748a6b7d9167678f9bc58d7ccb
Eric Tendian <erictendian@gmail.com>
2015-11-17 14:51:35 -0600

    Remove hash on modal close

--- public/main.js
+++ public/main.js
@@ -39,6 +39,10 @@ $('#directiveViewer form input').focus(function (event) {
   }
 });
 
+$('#directiveViewer').on('hidden.bs.modal', function () {
+    window.location.hash = '';
+});
+
 // GOOGLE ANALYTICS START
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
